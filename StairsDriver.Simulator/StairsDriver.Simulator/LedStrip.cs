f39af9205abbd9c0cba3003c884e2c4304c31499
Arkadiusz Neuman <arkadiusz.neuman@ialbatros.com>
2018-11-28 20:08:19 +0100

    Added same classes as in c++ project.

---
+++ StairsDriver.Simulator/StairsDriver.Simulator/LedStrip.cs
@@ -0,0 +1,166 @@
+﻿namespace StairsDriver.Simulator
+{
+    public class LedStrip
+    {
+        MillisMock millisMock;
+        int milisCountForFullBrightness;
+        bool isFadingPlanned;
+        bool brightnessGoingUp;
+        long millisStart;
+        int minLevel;
+        int maxLevel;
+        int brightnessToSet;
+        int MAX_LED_BRIGHTNESS = 4096;
+        int currentBrightness;
+        double previousTimeLeftPercent;
+        bool isFading;
+
+        public LedStrip(MillisMock millisMock, int milisCountForFullBrightness)
+        {
+            this.milisCountForFullBrightness = milisCountForFullBrightness;
+            this.millisMock = millisMock;
+        }
+
+        long millis()
+        {
+            return millisMock.Millis;
+        }
+
+        public void Fade(int brightnessPercent, int delay)
+        {
+            long currentMillisStart = millis();
+
+            if (this.isFadingPlanned && !this.brightnessGoingUp)
+            {
+                if (this.millisStart < currentMillisStart)
+                    return;
+            }
+
+            if (brightnessPercent > this.maxLevel)
+                brightnessPercent = this.maxLevel;
+            if (brightnessPercent < this.minLevel)
+                brightnessPercent = this.minLevel;
+
+            this.brightnessToSet = (int)(brightnessPercent * 1.0 * MAX_LED_BRIGHTNESS / 100);
+
+            this.brightnessGoingUp = this.brightnessToSet > this.currentBrightness;
+            this.millisStart = currentMillisStart;
+            this.isFadingPlanned = true;
+            this.previousTimeLeftPercent = 0;
+        }
+
+        void AddFadePlan(int brightnessPercent, int delay)
+        {
+            //if (this.fadePlan)
+            //	delete this.fadePlan;
+            //this.fadePlan = new FadeInfo(brightnessPercent, millis() + delay);
+        }
+
+        public void Update()
+        {
+            /*if (!this.isFading && this.fadePlan)
+            {
+                int delay = this.fadePlan.GetStartOnMillis() - millis();
+                if (delay >= 0)
+                    Fade(this.fadePlan.GetBrightnessPercent(), delay);
+
+                delete this.fadePlan;
+                this.fadePlan = NULL;
+            }*/
+
+            if (!this.isFadingPlanned)
+                return;
+
+            long currentMillis = millis();
+            if (currentMillis >= millisStart)
+            {
+                this.isFading = true;
+                long difference = currentMillis - millisStart;
+
+                double timeLeftPercent = difference * 1.0 * 100 / this.milisCountForFullBrightness;
+
+                if (timeLeftPercent - this.previousTimeLeftPercent >= 1)
+                {
+                    if (this.brightnessGoingUp)
+                        this.currentBrightness += (int)((timeLeftPercent - this.previousTimeLeftPercent) * MAX_LED_BRIGHTNESS / 100);
+                    else
+                        this.currentBrightness -= (int)((timeLeftPercent - this.previousTimeLeftPercent) * MAX_LED_BRIGHTNESS / 100);
+
+                    this.previousTimeLeftPercent = timeLeftPercent;
+
+                    if (timeLeftPercent >= 100 ||
+                        (brightnessGoingUp && currentBrightness >= brightnessToSet) ||
+                        (!brightnessGoingUp && currentBrightness <= brightnessToSet))
+                    {
+                        this.currentBrightness = this.brightnessToSet;
+                        this.isFadingPlanned = this.isFading = false;
+                        this.millisStart = 0;
+                    }
+
+                    SetPWM(this.currentBrightness);
+                }
+            }
+        }
+
+        private void SetPWM(int pwmValue)
+        {
+            if (pwmValue >= MAX_LED_BRIGHTNESS)
+                pwmValue = MAX_LED_BRIGHTNESS;
+            else if (pwmValue <= 0)
+                pwmValue = 0;
+            else
+                pwmValue = MAX_LED_BRIGHTNESS - pwmValue;
+        }
+
+        public bool IsBrightnessGoingUp()
+        {
+            return this.brightnessGoingUp;
+        }
+
+        public bool IsFading()
+        {
+            return this.isFading;
+        }
+
+        public bool IsFadePlanned()
+        {
+            return this.isFadingPlanned;
+        }
+
+        public double GetCurrentBrightness()
+        {
+            return this.currentBrightness;
+        }
+
+        public void SetMinLevel(int minLevel)
+        {
+            this.minLevel = minLevel;
+            Fade(this.minLevel, 0);
+        }
+
+        public void SetMaxLevel(int maxLevel)
+        {
+            this.maxLevel = maxLevel;
+        }
+
+        public int GetMinLevelPwm()
+        {
+            return (int)(this.minLevel * 1.0 * MAX_LED_BRIGHTNESS / 100);
+        }
+
+        public int GetMaxLevelPwm()
+        {
+            return (int)(this.maxLevel * 1.0 * MAX_LED_BRIGHTNESS / 100);
+        }
+
+        public bool IsFadedToMinLevel()
+        {
+            return GetCurrentBrightness() == GetMinLevelPwm();
+        }
+
+        public bool IsFadedToMaxLevel()
+        {
+            return GetCurrentBrightness() == GetMaxLevelPwm();
+        }
+    }
+}
---
+++ StairsDriver.Simulator/StairsDriver.Simulator/StairsLedDriver.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StairsDriver.Simulator
+{
+    public class StairsLedDriver
+    {
+        int timeForLedsSwitchedOn;
+        int delayForNextStairToSwitchOn;
+        int millisCountForFullBrightness;
+        int stairsCount;
+        private LedStrip[] ledStrips;
+        private MillisMock millisMock;
+        int state;
+        int STAIRS_OFF = 0, STAIRS_GO_UP = 1, STAIRS_GO_DOWN = 2, STAIRS_GO_UP_AND_DOWN = 3;
+        long timeOfLastSensorDetected;
+
+        public void Begin(MillisMock millisMock, int timeForLedsSwitchedOn, int delayForNextStairToSwitchOn, int millisCountForFullBrightness,
+            int stairsCount)
+        {
+            this.millisMock = millisMock;
+            this.timeForLedsSwitchedOn = timeForLedsSwitchedOn;
+            this.delayForNextStairToSwitchOn = delayForNextStairToSwitchOn;
+            this.millisCountForFullBrightness = millisCountForFullBrightness;
+            this.stairsCount = stairsCount;
+
+            this.ledStrips = new LedStrip[this.stairsCount];
+            for (int i = 0; i < this.stairsCount; i++)
+                this.ledStrips[i] = new LedStrip(millisMock, this.millisCountForFullBrightness);
+
+        }
+
+        private long millis()
+        {
+            return this.millisMock.Millis;
+        }
+
+        public void GoUp()
+        {
+            if ((this.state == STAIRS_GO_DOWN || this.state == STAIRS_OFF || ledStrips[0].IsFadedToMinLevel() || !ledStrips[0].IsBrightnessGoingUp())
+                )
+            {
+                bool isAnyFaded = false;
+
+                for (int i = 0; i < stairsCount; ++i)
+                {
+                    /*	if ((!ledStrips[i].IsFading() && !ledStrips[i].IsFadedToMaxLevel()) 
+                            )*/
+                    {
+                        ledStrips[i].Fade(100, i * this.delayForNextStairToSwitchOn);
+                        isAnyFaded = true;
+                    }
+                }
+
+                if (isAnyFaded)
+                {
+                    if (this.state == STAIRS_GO_DOWN)
+                        this.state = STAIRS_GO_UP_AND_DOWN;
+                    else
+                        this.state = STAIRS_GO_UP;
+                }
+            }
+
+            this.timeOfLastSensorDetected = millis();
+        }
+
+        public void GoDown()
+        {
+            if ((this.state == STAIRS_GO_UP || this.state == STAIRS_OFF || ledStrips[stairsCount - 1].IsFadedToMinLevel() || !ledStrips[stairsCount - 1].IsBrightnessGoingUp())
+                )
+            {
+                bool isAnyFaded = false;
+
+                for (int i = 0; i < stairsCount; ++i)
+                {
+                    int currentLedStrip = stairsCount - i - 1;
+                    /*if ((!ledStrips[currentLedStrip].IsFading() && !ledStrips[currentLedStrip].IsFadedToMaxLevel())
+                        )*/
+                    {
+                        ledStrips[currentLedStrip].Fade(100, i * this.delayForNextStairToSwitchOn);
+                        isAnyFaded = true;
+                    }
+                }
+
+                if (isAnyFaded)
+                {
+                    if (this.state == STAIRS_GO_UP)
+                        this.state = STAIRS_GO_UP_AND_DOWN;
+                    else
+                        this.state = STAIRS_GO_DOWN;
+                }
+            }
+
+            this.timeOfLastSensorDetected = millis();
+        }
+
+        public void Update()
+        {
+            for (int i = 0; i < this.stairsCount; ++i)
+            {
+                ledStrips[i].Update();
+            }
+
+            if (state > STAIRS_OFF)
+            {
+                if (millis() - this.timeOfLastSensorDetected > this.timeForLedsSwitchedOn)
+                {
+                    bool allStairsAreOff = true;
+
+                    if (state == STAIRS_GO_UP)
+                    {
+                        for (int i = 0; i < stairsCount; ++i)
+                        {
+                            if (ledStrips[i].IsFadePlanned())
+                                allStairsAreOff = false;
+                            else
+                                ledStrips[i].Fade(0, i * this
+                                    .delayForNextStairToSwitchOn);
+                        }
+                    }
+                    else if (state == STAIRS_GO_DOWN)
+                    {
+                        for (int i = 0; i < stairsCount; ++i)
+                        {
+                            if (ledStrips[stairsCount - i - 1].IsFadePlanned())
+                                allStairsAreOff = false;
+                            else
+                                ledStrips[stairsCount - i - 1]
+                                    .Fade(0, i * this.delayForNextStairToSwitchOn);
+                        }
+                    }
+                    else if (state == STAIRS_GO_UP_AND_DOWN)
+                    {
+                        int middleStair = stairsCount / 2;
+
+                        for (int i = middleStair; i >= 0; --i)
+                        {
+                            if (ledStrips[i].IsFadePlanned())
+                                allStairsAreOff = false;
+                            else
+                            {
+                                if (ledStrips[i].GetCurrentBrightness() > ledStrips[i].GetMinLevelPwm())
+                                {
+                                    ledStrips[i]
+                                        .Fade(0, (middleStair - i) * this.delayForNextStairToSwitchOn);
+                                }
+                            }
+
+                            if (ledStrips[stairsCount - i - 1].IsFadePlanned())
+                                allStairsAreOff = false;
+                            else
+                            {
+                                if (ledStrips[stairsCount - i - 1].GetCurrentBrightness() > ledStrips[stairsCount - i - 1].GetCurrentBrightness())
+                                {
+                                    ledStrips[stairsCount - i - 1]
+                                        .Fade(0, (middleStair - i) * this.delayForNextStairToSwitchOn);
+                                }
+                            }
+                        }
+                    }
+
+                    if (allStairsAreOff)
+                        this.state = STAIRS_OFF;
+                }
+            }
+        }
+
+        public void SetMinLevel(int minLevel)
+        {
+            for (int i = 0; i < this.stairsCount; i++)
+                this.ledStrips[i].SetMinLevel(minLevel);
+        }
+
+        public void SetMaxLevel(int maxLevel)
+        {
+            for (int i = 0; i < this.stairsCount; i++)
+                this.ledStrips[i].SetMaxLevel(maxLevel);
+        }
+    }
+}
